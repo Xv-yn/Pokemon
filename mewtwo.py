1c725c6c041d22d710bc0f8892724aee35b09dc0
OxKahi <waifumaximalist@gmail.com>
2024-07-06 23:09:06 +0800

    better fix conflict

--- mewtwo.py
+++ mewtwo.py
@@ -1,9 +1,17 @@
 """
 Mew
 """
+
+
 class Mewtwo:
-    attack = 150
+    attack = 100000000000000000000000000000000000000000000000000000000000000
     defence = 44
     spatk = 200
     spdef = 66
     spe = 299
+
+
+class MewTwoButGay:
+    attack = 0
+    defence = 0
+    gayness = 1000000000000000000000000000000000
